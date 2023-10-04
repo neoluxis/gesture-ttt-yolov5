dddc80dd690ceda9bfba540a882652649ebec9f8
Neolux Lee <neolux.lee@gmail.com>
2023-10-04 09:16:41 +0800

    edit training dataset

--- takePhoto.py
+++ takePhoto.py
@@ -47,7 +47,7 @@ def get_sets():
                         type=str, help='output txt label path')
     opt = parser.parse_args()
 
-    trainval_percent = 0.25
+    trainval_percent = 0.98
     train_percent = 0.20
     xmlfilepath = opt.xml_path
     txtsavepath = opt.txt_path
